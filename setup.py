3f24529a55133f800aa1270bb891831c57707db9
SABARI SARAVANAN M <58065006+sabari-saravanan-m@users.noreply.github.com>
2021-05-02 13:42:36 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='NI8452',
+version='0.1.1',
+description='Installation Package of NI-USB8452',
+url='#',
+author='SABARI SARAVANAN M',
+author_email='saravana.braven@gmail.com',
+license='SN',
+packages=["NI8452"],
+zip_safe=False)
